5a94b9a85c67e36ee291a2734646b8f473e7263b
Tasnádi Zoltán <tasnadi98@freemail.hu>
2020-07-31 18:20:06 +0300

    Implemented TypicalDetector

--- src/detector/typical.rs
+++ src/detector/typical.rs
@@ -24,7 +24,7 @@ impl TypicalStatus {
 
 /// This detects if there isn't a comma after a word that is tipically introduces clauses.
 ///
-/// Example: Reméljük, nem esett baja. (there is a "hogy" missing in the sentence)
+/// Example: Reméljük, nem esett baja. (there is an implicit "hogy")
 ///  We hope that he/she/it wasn't hurt.
 pub struct TypicalDetector {
     settings: TypicalSettings,
@@ -32,10 +32,36 @@ pub struct TypicalDetector {
 }
 
 impl TypicalDetector {
-    fn new(settings: TypicalSettings) -> TypicalDetector {
+    pub fn new(settings: TypicalSettings) -> TypicalDetector {
         TypicalDetector {
             status: TypicalStatus::new(settings.words.len()),
             settings
+        }
+    }
+
+    fn move_cursor_forward(&mut self, current_token: &PlainTextToken, tokens: &Lexer<PlainTextToken>) {
+        self.status.col += tokens.slice().chars().count() + 1;
+        if *current_token == PlainTextToken::NewLine {
+            self.status.col = 1;
+            self.status.row += 1;
+        }
+    }
+
+    fn get_mistake_for_word(&self, pos1: usize) -> (usize, usize, Mistake) {
+        (
+            self.status.row,
+            self.status.col,
+            Mistake::new_dyn(
+                format!("mondatokba, melyekben szerepel a(z) \"{}\" szó, gyakran teszünk vesszőt.", self.settings.words[pos1]),
+                self.settings.probs[pos1]
+            )
+        )
+    }
+
+    fn set_active_word(&mut self, word: &str) {
+        let index = self.settings.words.iter().position(|a| a == word);
+        if let Some(pos) = index {
+            self.status.word_active[pos].set(true);
         }
     }
 }
@@ -48,6 +74,68 @@ impl Detector for TypicalDetector {
     }
 
     fn detect_errors_in_row(&mut self, tokens: &mut Lexer<PlainTextToken>) -> Vec<(usize, usize, Mistake)> {
-        unimplemented!()
+        let mut errors = Vec::new();
+        while let Some(token) = tokens.next() {
+            let lowercase = String::from(tokens.slice()).to_lowercase();
+
+            if token == PlainTextToken::EndOfSentence {
+                for (pos, _) in self.status.word_active.iter().enumerate().filter(|(_, a)| a.get()) {
+                    errors.push(self.get_mistake_for_word(pos));
+                }
+            }
+
+            self.move_cursor_forward(&token, tokens);
+            self.set_active_word(lowercase.as_str());
+
+            if token == PlainTextToken::Comma || token == PlainTextToken::EndOfSentence {
+                self.status.word_active.iter().for_each(|a| a.set(false));
+            }
+        }
+        errors
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use logos::Logos;
+
+    use crate::detector::TypicalDetector;
+    use crate::model::{PlainTextToken, TypicalSettings};
+    use crate::traits::Detector;
+
+    #[test]
+    fn empty_str() {
+        let mut sut = TypicalDetector::new(TypicalSettings { words: Vec::new(), probs: Vec::new()});
+        let mut tokens = PlainTextToken::lexer("");
+        let errors = sut.detect_errors(&mut tokens);
+
+        assert_eq!(errors.len(), 0);
+    }
+
+    #[test]
+    fn comma_provided() {
+        let mut sut = TypicalDetector::new(TypicalSettings { words: vec![String::from("remélem"),], probs: vec![1.0]});
+        let mut tokens = PlainTextToken::lexer("Remélem, jól van.");
+        let errors = sut.detect_errors(&mut tokens);
+
+        assert_eq!(errors.len(), 0);
+    }
+
+    #[test]
+    fn semicolon_provided() {
+        let mut sut = TypicalDetector::new(TypicalSettings { words: vec![String::from("remélem"),], probs: vec![1.0]});
+        let mut tokens = PlainTextToken::lexer("Remélem; jól van.");
+        let errors = sut.detect_errors(&mut tokens);
+
+        assert_eq!(errors.len(), 0);
+    }
+
+    #[test]
+    fn comma_missing() {
+        let mut sut = TypicalDetector::new(TypicalSettings { words: vec![String::from("remélem"),], probs: vec![1.0]});
+        let mut tokens = PlainTextToken::lexer("Remélem jól van.");
+        let errors = sut.detect_errors(&mut tokens);
+
+        assert_eq!(errors.len(), 1);
     }
 }
--- src/model/typical_settings.rs
+++ src/model/typical_settings.rs
@@ -14,14 +14,14 @@ impl TypicalSettings {
         let content = fs::read_to_string(filename).expect(format!("File not found: \"{}\"", filename).as_str());
         let rows = content.split("\n");
 
-        let mut first_words = Vec::new();
+        let mut words = Vec::new();
         let mut probs = Vec::new();
 
         for row in rows {
             let cols = row.split(";").collect::<Vec<&str>>();
 
             if cols.len() > 1 {
-                first_words.push(String::from(cols[0]));
+                words.push(String::from(cols[0]));
                 probs.push(cols[1].trim().parse::<f64>().unwrap());
             }
         }
